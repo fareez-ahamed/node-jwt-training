a8321bd793b0ea347d41bf1de5f9753cb09ff93d
Fareez Ahamed <fareez.ahamed@gmail.com>
2023-08-22 16:49:02 +0530

    Step 4: HTTP Server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const fs = require("fs");
+const http = require("http");
 
-fs.readFile("lorem.txt", (err, data) => {
-  if (err) {
-    console.log("Got error", err);
-    return;
-  }
-  console.log(data.toString());
+const server = http.createServer((req, res) => {
+  const name = req.url.substring(req.url.lastIndexOf("/") + 1);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`Hello ${name}`);
+});
+
+server.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
